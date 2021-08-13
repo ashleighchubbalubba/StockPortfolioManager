8f575c6d01b668c697e33005c7012a1a2e59470e
Ashleigh Chung <44147739+ashleighchung@users.noreply.github.com>
2021-08-13 10:24:17 -0400

    Add files via upload

---
+++ StockPortfolioManager/src/main/java/com/citi/training/StockPortfolioManager/StockPortfolioManagerApplication.java
@@ -0,0 +1,13 @@
+package com.citi.training.StockPortfolioManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockPortfolioManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockPortfolioManagerApplication.class, args);
+	}
+
+}
---
+++ StockPortfolioManager/src/test/java/com/citi/training/StockPortfolioManager/StockPortfolioManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citi.training.StockPortfolioManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockPortfolioManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
